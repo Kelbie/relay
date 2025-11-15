375110985116358cf989d6277e69fcb64b61bef2
pippellia-btc <pippellia@gmail.com>
2025-11-15 15:39:17 +0100

    simpler error definitions

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -16,23 +16,24 @@ import (
 )
 
 var (
-	Global       string = "globalPagerank"
-	Personalized string = "personalizedPagerank"
-	Followers    string = "followerCount"
+	Global       = "globalPagerank"
+	Personalized = "personalizedPagerank"
+	Followers    = "followerCount"
 )
 
 var (
-	ErrInvalidSource     error = errors.New("invalid source")
-	ErrInvalidSort       error = errors.New("invalid sort")
-	ErrInvalidTarget     error = errors.New("invalid target")
-	ErrInvalidTargets    error = errors.New("invalid targets")
-	ErrInvalidLimit      error = errors.New("invalid limit")
-	ErrInvalidSearch     error = errors.New("invalid search")
-	ErrBadlyFormattedKey error = errors.New("badly formatted key")
-	ErrMultipleParams    error = errors.New("too many parameters of the same type")
+	ErrInvalidSource     = errors.New("invalid source")
+	ErrInvalidSort       = errors.New("invalid sort")
+	ErrInvalidTarget     = errors.New("invalid target")
+	ErrInvalidTargets    = errors.New("invalid targets")
+	ErrInvalidLimit      = errors.New("invalid limit")
+	ErrInvalidSearch     = errors.New("invalid search")
+	ErrBadlyFormattedKey = errors.New("badly formatted key")
+	ErrMultipleParams    = errors.New("too many parameters of the same type")
 
-	ErrInternal  error = errors.New("internal error")
-	ErrNoCredits error = errors.New("you don't have enough credits to fulfil the request. Send us a DM and we'll give you a top-up for free!")
+	ErrUnsupportedArgs = errors.New("unsupported args")
+	ErrInternal        = errors.New("internal error")
+	ErrNoCredits       = errors.New("you don't have enough credits to fulfil the request. Send us a DM and we'll give you a top-up for free!")
 )
 
 // Service encapsulates the business logic of the Vertex services.
